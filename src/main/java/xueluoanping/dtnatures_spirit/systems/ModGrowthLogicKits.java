2e04e0bc7a5c8716f7430e3388c8e7692d4d9b2a
joe-vettek <my1233215@126.com>
2024-06-07 19:46:52 +0800

    update

--- src/main/java/xueluoanping/dtnatures_spirit/systems/ModGrowthLogicKits.java
+++ src/main/java/xueluoanping/dtnatures_spirit/systems/ModGrowthLogicKits.java
@@ -4,10 +4,7 @@ import com.ferreusveritas.dynamictrees.api.registry.Registry;
 import com.ferreusveritas.dynamictrees.growthlogic.GrowthLogicKit;
 import net.minecraft.resources.ResourceLocation;
 import xueluoanping.dtnatures_spirit.DTNaturesSpirit;
-import xueluoanping.dtnatures_spirit.systems.growthlogic.BifurcatedTreeLogic;
-import xueluoanping.dtnatures_spirit.systems.growthlogic.WisteriaTreeLogic;
-import xueluoanping.dtnatures_spirit.systems.growthlogic.CrookedPalmLogic;
-import xueluoanping.dtnatures_spirit.systems.growthlogic.RedwoodLogic;
+import xueluoanping.dtnatures_spirit.systems.growthlogic.*;
 
 
 public class ModGrowthLogicKits {
@@ -15,8 +12,10 @@ public class ModGrowthLogicKits {
     public static final GrowthLogicKit HEMLOCK = new RedwoodLogic(new ResourceLocation(DTNaturesSpirit.MOD_ID, "hemlock"));
     public static final GrowthLogicKit WISTERIA = new WisteriaTreeLogic(new ResourceLocation(DTNaturesSpirit.MOD_ID, "wisteria"));
     public static final GrowthLogicKit BIFURCATED = new BifurcatedTreeLogic(new ResourceLocation(DTNaturesSpirit.MOD_ID, "bifurcated"));
+    public static final GrowthLogicKit LARGE_CROWN = new LargeCrownTreeLogic(new ResourceLocation(DTNaturesSpirit.MOD_ID, "large_crown"));
 
     public static void register(final Registry<GrowthLogicKit> registry) {
         registry.registerAll(CROOKED_PALM, HEMLOCK, WISTERIA, BIFURCATED);
+        registry.register(LARGE_CROWN);
     }
 }
---
+++ src/main/java/xueluoanping/dtnatures_spirit/systems/growthlogic/LargeCrownTreeLogic.java
@@ -0,0 +1,66 @@
+package xueluoanping.dtnatures_spirit.systems.growthlogic;
+
+import com.ferreusveritas.dynamictrees.api.configuration.ConfigurationProperty;
+import com.ferreusveritas.dynamictrees.growthlogic.GrowthLogicKitConfiguration;
+import com.ferreusveritas.dynamictrees.growthlogic.PalmGrowthLogic;
+import com.ferreusveritas.dynamictrees.growthlogic.context.DirectionManipulationContext;
+import com.ferreusveritas.dynamictrees.systems.GrowSignal;
+import com.ferreusveritas.dynamictrees.tree.species.Species;
+import com.ferreusveritas.dynamictrees.util.CoordUtils;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+// Thanks to maxhyper
+// signal_energy controls the maximum trunk length.
+// The value of each position in the six directions of probMap affects the initial branch size,
+// but in the end, only values greater than 0 will always grow unless the fertility is exhausted.
+// Because each growth pulse is sent throughout the tree along the trunk.
+public class LargeCrownTreeLogic extends PalmGrowthLogic {
+
+
+    public LargeCrownTreeLogic(ResourceLocation registryName) {
+        super(registryName);
+    }
+
+    @Override
+    protected GrowthLogicKitConfiguration createDefaultConfiguration() {
+        return super.createDefaultConfiguration();
+                // .with(TURNING_LENGTH, 0);
+    }
+
+    @Override
+    protected void registerProperties() {
+        // this.register( TURNING_LENGTH);
+    }
+
+    @Override
+    public int[] populateDirectionProbabilityMap(GrowthLogicKitConfiguration configuration, DirectionManipulationContext context) {
+        final Species species = context.species();
+        final Level world = context.level();
+        final GrowSignal signal = context.signal();
+        int[] probMap = new int[]{0, 2, 0, 0, 0, 0};
+        final BlockPos pos = context.pos();
+        var rootPos = signal.rootPos;
+        Direction originDir = signal.dir.getOpposite();
+
+        int currentHeight = signal.numSteps + 1;
+        long seed = CoordUtils.coordHashCode(signal.rootPos, 3) + ((ServerLevel) world).getSeed();
+        Random random = new Random(seed);
+
+        if (currentHeight>20){
+
+            probMap = new int[]{0, 0, 10, 10, 10, 10};
+            probMap[originDir.ordinal()]=0;
+        }
+
+        return probMap;
+    }
+
+}
